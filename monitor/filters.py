52f910278c53185b043bf24b880c71f6c08408cd
Himanshu Yadav <y.himanshurockstar@gmail.com>
2026-09-22 01:40:35 +0530

    Apply configured article-type filters

--- monitor/filters.py
+++ monitor/filters.py
@@ -1,4 +1,5 @@
 from monitor.normalize import normalize_text
+
 FRONT_MATTER_TERMS=("cover","front matter","back matter","contents","table of contents","editorial board","masthead","contributors")
 
 def keep_article(journal, article):
@@ -6,4 +7,11 @@ def keep_article(journal, article):
     title=normalize_text(article.title)
     if f.get("exclude_front_back_matter") and any(x in title for x in FRONT_MATTER_TERMS):
         return False
+    keep_types=f.get("default_keep_article_types") or f.get("default_keep")
+    if keep_types:
+        raw=normalize_text(article.item_type or "")
+        normalized=raw.replace("_"," ").replace("-"," ")
+        allowed={normalize_text(x).replace("_"," ").replace("-"," ") for x in keep_types}
+        if normalized and normalized not in allowed:
+            return False
     return True
