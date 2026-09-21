3cb56d40469d257c63643f3573dd2fa3f3805cdd
Himanshu Yadav <y.himanshurockstar@gmail.com>
2026-09-22 01:06:29 +0530

    Add monitor/models.py

---
+++ monitor/models.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, asdict
+from typing import Optional
+
+@dataclass
+class Article:
+    journal: str
+    journal_id: str
+    title: str
+    url: str
+    doi: Optional[str] = None
+    published: Optional[str] = None
+    updated: Optional[str] = None
+    item_type: Optional[str] = None
+    source: Optional[str] = None
+    issue: Optional[str] = None
+
+    def to_dict(self):
+        return asdict(self)
